b08b76aca6ec8bc89d80bfe8d8593448d0422a4d
Yuheng Su <gipsyh.icu@gmail.com>
2024-07-03 08:31:01 +0800

    update

--- examples/counter/10bit/counter.v
+++ examples/counter/10bit/counter.v
@@ -1,13 +1,15 @@
+`define W 10
+
 module main(
     input wire clk,
 );
-    reg [9:0] count;
-    initial count = 10'b0000000000;
+    reg [`W-1:0] count;
+    initial count = `W'b0000000000;
     always @(posedge clk) begin
         count <= count + 1;
     end
 
-    wire prop = (count != 10'b1111111111);
+    wire prop = (count != `W'b1111111111);
     assert property ( prop );
 
 endmodule
